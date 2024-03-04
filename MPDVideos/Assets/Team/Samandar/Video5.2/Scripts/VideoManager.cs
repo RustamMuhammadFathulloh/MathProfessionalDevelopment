36125bf2a3ab8167aa763454ad605f2324194055
Samandar2001 <samandaribrohimov2001@gmail.com>
2024-03-04 08:43:37 +0000

    Page_2

--- MPDVideos/Assets/Team/Samandar/Video5.2/Scripts/VideoManager.cs
+++ MPDVideos/Assets/Team/Samandar/Video5.2/Scripts/VideoManager.cs
@@ -11,26 +11,29 @@ namespace Samandar
 {
    public class VideoManager : Introduction
     {
+        public GameObject transition_1;
+        public GameObject transition_2;
         public List<GameObject> Pages;
         //public List<float> PageTime;
         public List<GameObject> Page_2;
-        public GameObject transition;
+        public List<GameObject> Page_3;
         public List<AudioSource> Audio;
-        public UnityEvent PageEvent_2;
+        public UnityEvent PageEvent_1;
         private void Start()
-        {
-            StartCoroutine(PageController_2());
-            StartCoroutine(AudioController());
-        }
-        public void Challenge()
         {
             
         }
-        public void Transition()
+        public void ChallengePage_2()
         {
-            transition.SetActive(true);
+            StartCoroutine(PageController_2());
+            StartCoroutine(AudioControllerPage_2());
         }
-        IEnumerator AudioController() 
+        public void ChallengePage_3()
+        {
+            StartCoroutine(PageController_3());
+            StartCoroutine(AudioControllerPage_3());
+        }        
+        IEnumerator AudioControllerPage_2() 
         {
             yield return new WaitForSeconds(0.5f);
             Audio[0].Play();
@@ -38,6 +41,15 @@ namespace Samandar
             Audio[1].Play();
             yield return new WaitForSeconds(6.4f);
             Audio[2].Play();
+        }
+        IEnumerator AudioControllerPage_3()
+        {            
+            Audio[3].Play();
+            yield return new WaitForSeconds(16);
+            Audio[4].Play();
+            yield return new WaitForSeconds(12.5f);
+            Audio[5].Play();
+            yield return new WaitForSeconds(4.5f);
         }
         IEnumerator PageController_2()
         {
@@ -99,8 +111,53 @@ namespace Samandar
             Page_2[19].GetComponent<Image>().DOFillAmount(1, 0);
             Page_2[19].GetComponent<Image>().DOFillAmount(0, 0.2f);
             yield return new WaitForSeconds(7);
-            PageEvent_2.Invoke();
-        }
+            transition_1.SetActive(true);
+            yield return new WaitForSeconds(1.5f);
 
+            PageEvent_1.Invoke();
+        }
+        IEnumerator PageController_3()
+        {
+            Pages[0].SetActive(false);
+            Pages[1].SetActive(true);
+            Page_3[0].SetActive(true);
+            yield return new WaitForSeconds(16);
+            Page_3[1].SetActive(true);
+            Page_3[1].GetComponent<RectTransform>().DOAnchorPosX(-578.8f, 1);
+            Page_3[2].SetActive(true);
+            Page_3[3].SetActive(true);
+            yield return new WaitForSeconds(1.5f);
+            Page_3[8].SetActive(true);
+            Page_3[4].SetActive(true);
+            Page_3[4].GetComponent<RectTransform>().DOScale(1,0.7f);
+            yield return new WaitForSeconds(0.2f);
+            Page_3[5].SetActive(true);
+            Page_3[5].GetComponent<RectTransform>().DOScale(1, 0.7f);
+            yield return new WaitForSeconds(0.2f);
+            Page_3[6].SetActive(true);
+            Page_3[6].GetComponent<RectTransform>().DOScale(1, 0.7f);
+            yield return new WaitForSeconds(0.2f);
+            Page_3[7].SetActive(true);
+            Page_3[7].GetComponent<RectTransform>().DOScale(1, 0.7f);
+            yield return new WaitForSeconds(1.2f);
+            Page_3[9].SetActive(true);
+            yield return new WaitForSeconds(10);
+            Page_3[10].SetActive(true);
+            Page_3[10].GetComponent<RectTransform>().DOScale(1, 0.7f);
+            yield return new WaitForSeconds(0.7f);
+            Page_3[14].SetActive(true);
+            Page_3[11].SetActive(true);
+            Page_3[11].GetComponent<Image>().DOFillAmount(1, 0.2f);
+            yield return new WaitForSeconds(0.2f);
+            Page_3[12].SetActive(true);
+            Page_3[12].GetComponent<Image>().DOFillAmount(1, 0.5f);
+            Page_3[13].SetActive(true);
+            Page_3[13].GetComponent<Image>().DOFillAmount(1, 0.5f);
+            yield return new WaitForSeconds(0.5f);
+            Page_3[15].SetActive(true);
+            Page_3[15].GetComponent<Image>().DOFillAmount(1, 0.2f);
+            Page_3[16].SetActive(true);
+            Page_3[16].GetComponent<Image>().DOFillAmount(1, 0.2f);
+        }
    }
 }
